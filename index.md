80822a60ba6d0e43adc6a296167ff06b4dca8790
GJablanczy <gabe.jablanczy@isobar.com>
2016-11-23 11:21:19 -0500

    creating of jekyll portfolio site implementation

---
+++ index.md
@@ -0,0 +1,44 @@
+---
+# You don't need to edit this file, it's empty on purpose.
+# Edit theme's home layout instead if you wanna make some changes
+# See: https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+layout: default
+---
+<div id="cmscontent">
+  <div class="top">
+    <h2>Front End Web Developer currently employed at <a id="link1" class="link" href="http://www.isobar.com/us/home">Isobar</a>, a digital marketing agency working on the cutting edge of e-technology (internet things). I'm a pro at CSS3, HTML5, Javascript...<br />Swag swag swag swag swag <em>it's lit</em> swag swag swag swag swag swag. Check me out on codepen.</h2>
+  </div>
+  <p data-height="780" data-theme-id="0" data-slug-hash="YWgkZR" data-default-tab="result" data-user="gjablanczy" data-embed-version="2" data-pen-title="svg scorpion loader" class="codepen">See the Pen <a href="http://codepen.io/gjablanczy/pen/YWgkZR/">svg scorpion loader</a> by Gabriel Jablanczy (<a href="http://codepen.io/gjablanczy">@gjablanczy</a>) on <a href="http://codepen.io">CodePen</a>.</p>
+  <script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
+</div>
+<div class="cv">
+  <div class="cv-title-wrap">
+    <div class="cv-title-inner-wrap">
+      <h2 class="cv-title">CV</h2>
+      <div class="arrow">&larr;</div>
+    </div>
+  </div>
+  <div class="job">
+    <h3>
+      <a id="link0" class="link" href="http://www.isobar.com/us/home" target="_blank">Isobar</a>
+    </h3>
+    <h4>2014 - present</h4>
+    <p>Award winning online marketing and enterprise level e-commerce experiences.<br />Accounts: Adidas, Lego, Royal Caribbean, <a id="link6" class="link" href="https://exoticcars.enterprise.com/en/home.html" target="_blank">Enterprise</a></p>
+    <div class="awards">
+      <h4>Award winning work at Isobar:</h4>
+      <h4><a id="link7" class="link" href="https://exoticcars.enterprise.com/en/home.html" target="_blank">Enterprise Exotic Cars</a></h4>
+      <p><em>Official Nominee</em><br />The Webby Awards - July 2016</p>
+      <h4><a id="link5" class="link" href="http://www.nick.com/legoelves/" target="_blank">Lego Elves</a></h4>
+      <p><em>Gold General Website</em><br />W3 Awards - November 2015</p>
+      <h4><a id="link3" class="link" href="http://www.isobar.com/us/en/work/adidas-stellasport/" target="_blank">adidas Stella Sport</a></h4>
+      <p><em>Webaward for Outstanding Achievement in Web Development</em><br />Web Marketing Association - September 2015</p>
+      <h4><a id="link4" class="link" href="http://www.isobar.com/us/en/work/adidas-stellasport/" target="_blank">adidas Stella Sport</a></h4>
+      <p><em>Silver Hatch Awards&nbsp;2 x</em><br />Hatch Awards - October 2015</p>
+    </div>
+  </div>
+  <div class="job">
+    <h3><a id="link2" class="link" href="https://www.unboundcommerce.com/" target="_blank">Unbound Commerce</a></h3>
+    <h4>2012 -2014</h4>
+    <p>Providing mobile dedicated e-commerce web sites. <br /> Accounts: Monster Products, Icebreaker, <a id="link8" class="link" href="http://m.finishline.com/" target="_blank">Finishline</a>, Jomashop</p>
+  </div>
+</div>
---
+++ js/core/script.js
@@ -0,0 +1,149 @@
+window.requestAnimFrame = (function(){
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){window.setTimeout(callback, 1000 / 60);};
+})();
+var COLOR = {
+  r : 110,
+  g : 1,
+  b : 250,
+  rdown : false,
+  gdown : false,
+  bdown : false,
+  font: '',
+  themes: [{
+    bg: '#BAC1B8',
+    cp: '#D64933'
+  }, {
+    cp: '#E55381',
+    bg: '#190B28'
+  }, {
+    cp: '#797D81',
+    bg: '#EFB0A1'
+  }, {
+    cp: '#38023B',
+    bg: '#CCFFCB'
+  }, {
+    cp: '#FED766',
+    bg: '#FE4A49'
+  }, {
+    cp: '#6D3D14',
+    bg: '#CDC5B4'
+  }, {
+    cp: '#38AECC',
+    bg: '#022F40'
+  }, {
+    cp: '#6F5B58',
+    bg: '#F6D6BA'
+  }, {
+    cp: '#FFCBDD',
+    bg: '#3E000C'
+  }, {
+    cp: '#521945',
+    bg: '#EAF2EF'
+  }, {
+    bg: '#D64933',
+    cp: '#BAC1B8'
+  }, {
+    cp: '#190B28',
+    bg: '#E55381'
+  }, {
+    cp: '#EFB0A1',
+    bg: '#797D81'
+  }, {
+    cp: '#CCFFCB',
+    bg: '#38023B'
+  }, {
+    cp: '#FE4A49',
+    bg: '#FED766'
+  }, {
+    cp: '#CDC5B4',
+    bg: '#6D3D14'
+  }, {
+    cp: '#022F40',
+    bg: '#38AECC'
+  }, {
+    cp: '#F6D6BA',
+    bg: '#6F5B58'
+  }, {
+    cp: '#3E000C',
+    bg: '#FFCBDD'
+  }, {
+    cp: '#EAF2EF',
+    bg: '#521945'
+  }],
+  randomize: function() {
+    return Math.floor((Math.random() * COLOR.themes.length));
+  },
+  crazylinks: function(thelink) {
+    thelink.draw = function() {
+      window.requestAnimFrame(function() {
+        if(COLOR.rdown == true){
+          COLOR.r < 1 ? COLOR.rdown = false : COLOR.r = COLOR.r - 4;
+        }else{
+          COLOR.r > 240 ? COLOR.rdown = true : COLOR.r = COLOR.r + 4;
+        }
+        if(COLOR.gdown == true){
+          COLOR.g < 1 ? COLOR.gdown = false : COLOR.g = COLOR.g - 4;
+        }else{
+          COLOR.g > 240 ? COLOR.gdown = true : COLOR.g = COLOR.g + 4;
+        }
+        if(COLOR.bdown == true){
+          COLOR.b < 1 ? COLOR.bdown = false : COLOR.b = COLOR.b - 4;
+        }else{
+          COLOR.b > 240 ? COLOR.bdown = true : COLOR.b = COLOR.b + 4;
+        }
+        thelink.style.color = 'rgb(' + COLOR.r + ',' + COLOR.g + ',' + COLOR.b + ')';
+
+        if (thelink.stopanimation) {
+          thelink.style.color = COLOR.font;
+          //return;
+
+        } else {
+          thelink.draw();
+        }
+      });
+    }
+  },
+  links: function() {
+    var anchortags = document.getElementsByClassName('link');
+    var anchoramount = anchortags.length;
+    for (var i = 0; i < anchoramount; i++) {
+      var thelink = document.getElementById('link' + i);
+      COLOR.crazylinks(thelink);
+      thelink.addEventListener('mouseover', function(e) {
+        this.stopanimation = false;
+        this.draw();
+      }, false)
+      thelink.addEventListener('mouseout', function(e) {
+        this.stopanimation = true;
+        this.style.color = COLOR.font;
+      }, false)
+    }
+  },
+  buildstyle: function() {
+    var randomThemeNum = COLOR.randomize();
+    COLOR.font = COLOR.themes[randomThemeNum].cp;
+    var css = '#wrap { background: ' + COLOR.themes[randomThemeNum].bg + '; }' +
+      'body * { color:' + COLOR.font + ' }';
+
+    head = document.head || document.getElementsByTagName('head')[0],
+      style = document.createElement('style');
+    style.type = 'text/css';
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+    head.appendChild(style);
+  },
+  init: function() {
+    COLOR.buildstyle();
+    COLOR.links();
+
+    //Reload page if keystone is being shit.
+    if(document.getElementById("cmscontent") == null){
+      window.location.href = window.location.href;
+    }
+  }
+};
+window.addEventListener('load', COLOR.init, false)
+
